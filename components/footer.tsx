d9670522d1ff6abc186ac1fb59cf2c47a56dd1ef
HypotheticalWhale <samueltan.9803@gmail.com>
2023-12-01 22:42:02 +0800

    fix apos

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2023 Samuel&apos; World Inc. All rights reserved.
+          &copy; 2023 Samuel&apos;s World Inc. All rights reserved.
         </p>
       </div>
     </footer>
